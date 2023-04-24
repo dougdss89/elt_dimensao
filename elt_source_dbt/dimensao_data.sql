8b8c3af6966d8d1513786fb1aab2b0c41afc0f44
dougdss89 <skullfox99@gmail.com>
2023-04-24 16:12:40 -0300

    finalizando dimensao data

--- elt_source_dbt/dimensao_data.sql
+++ elt_source_dbt/dimensao_data.sql
@@ -12,7 +12,7 @@ select
 	orderdate,
 	year(orderdate) as orderyear,
 	'FY' + cast(year(orderdate) as varchar(5)) as fiscalyear,
-	datename(mm,orderdate) as month_name,
+	datename(mm,orderdate) as monthname,
 	substring(cast(datename(m, orderdate) as varchar(10)),1,3) as monthabrv,
 	datepart(m,orderdate) as monthnum,
 	case
@@ -23,6 +23,10 @@ select
 	end as quartermonth,
 	datename(w, orderdate) as weekname,
 	substring(cast(datename(w, orderdate) as varchar(10)),1,3) as weeknameabrv,
+	case 
+		when cast(datename(w, orderdate) as varchar(10)) = 'Saturday' or cast(datename(w, orderdate) as varchar(10)) = 'Sunday' then 'Weekend'
+		else 'Weekday'
+	end as isweekday,
 	datename(ww, orderdate) as weeknuminyear,
 	datepart(d, orderdate) as daynum,
 	case 
@@ -33,14 +37,44 @@ select
 		when cast(datename(w, orderdate) as varchar(10)) = 'monday' then 'Yes'
 		else 'No'
 	end as isweekbegin,
+	case
+		when cast(datename(w, orderdate) as varchar(3)) like 'mon%' then cast(orderdate as date)
+	end as weekbegindate,
 	upper(cast(year(orderdate) as varchar(5)) + '_' + substring(cast(datename(m, orderdate) as varchar(3)),1,3)) as yearmonth,
+	cast(datepart(YYYY, orderdate) as varchar(5)) + '' +  cast(month(orderdate) as varchar(5)) as yearmonthnum,
+	eomonth(orderdate) as lastdayofmonth,
+	case
+		when datepart(d, orderdate) = datepart(d, eomonth(orderdate)) then 'Yes'
+		--when datepart(m, orderdate) = 2 and datepart(d, orderdate) between 28 and 29 then 'Yes'
+	else 'No'
+	end as islastdayofmonth,
 	case	
 		when year(orderdate) % 4 = 0 or year(orderdate) % 400 = 0 and year(orderdate) % 100 <> 0  then 'Yes'
 	else 'No'
 	end as leapyear
+from elt_dimdate),
 
-	
-from elt_dimdate)
+normaliza_data as(
 
-select * from cria_dimdate
-order by orderdate;
+	select 
+		orderdate,
+		orderyear,
+		monthname,
+		monthabrv,
+		monthnum
+		quartermonth,
+		yearmonth,
+		yearmonthnum,
+		lastdayofmonth,
+		weekname,
+		weeknameabrv,
+		isweekday,
+		daynum,
+		isweekbegin,
+		ismonthbegin,
+		islastdayofmonth,
+		leapyear
+	from cria_dimdate)
+
+select * from normaliza_data
+order by orderdate
