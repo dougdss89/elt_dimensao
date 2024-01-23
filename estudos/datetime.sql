062d61d7c9ecf69a75b3b9a93b511bee3fd46106
dougdss89 <skullfox99@gmail.com>
2024-01-23 18:31:28 -0300

    sql commit datetime estudos

--- estudos/datetime.sql
+++ estudos/datetime.sql
@@ -106,3 +106,111 @@ select custid, orderid, orderdate -- essa query acusa falha
 from sales.Orders
 where orderdate = '2020/27/08';
 go
+
+set language us_english
+
+select custid, orderid, orderdate -- essa query acusa falha
+from sales.Orders
+where orderdate = '2020/08/27';
+go
+
+--IDENTIFY WEEK DAYS
+
+/*
+o "problema" do idioma é que ele determina o formato da data 
+e junto, o primeiro dia da semana
+
+Então as funções datepart e dateformat, serão afetadas
+
+também não é aconselhável alterar a configuração geral de idioma, para não
+alterar o caches de query plan e cálculos de sessões com outros idiomas.
+
+*/
+select * from sys.syslanguages
+-- na minha versão começa no domingo
+select datepart(WEEKDAY, SYSDATETIME()) as weekdaynumber;
+select DATENAME(WEEKDAY, getdate())
+-- o ideal do cálculo de data é rodar a função datefirst para calcular corretamente.
+select @@DATEFIRST;
+
+select
+	orderdate,
+	case
+		when DATENAME(dw, orderdate) like 'monday' then DATEPART(dw, orderdate)
+	end as 'weekname'
+from Sales.Orders;
+
+use TSQLV6;
+select
+	orderdate, 
+	datename(weekday, orderdate),
+	DATEPART(weekday, orderdate) as daynum 
+from sales.Orders
+where DATEPART(weekday, orderdate) = @@DATEFIRST % 7 +1;
+
+-- domingo com primeiro dia
+
+select DATEDIFF(day, '19000107', sysdatetime()) % 7 + 1;
+
+select DATEPART(weekday, sysdatetime()) -- domingo já é considerado o 1º dia
+
+select * from sys.syslanguages;
+
+use AdventureWorks2019;
+select 
+	orderdate,
+	datename(dw, orderdate) as weekname,
+	datepart(dw, orderdate) as daynumber,
+	case
+		when datename(dw, orderdate) = 'monday' then 'week begin'
+		when datepart(dw, orderdate) in(7,1) then 'weekend'
+	else 'week'
+	end as isweekbegin
+from Sales.SalesOrderHeader;
+
+-- WEEKDAY FIXO
+
+/*
+o que acontece é o seguinte:
+caso a linguagem do SQL Server mude, o dia da semana @@datefirst, pode mudar.
+para manter fixo, isso é, começando na segunda, existe uma solução.
+
+com a seguinte query abaixo, é possível.
+
+A query torna se torna neutra ao idioma utilizado, considerando a segunda como o 1º dia, em qualquer cenário.
+
+Mesmo em cenários onde o domingo é o primeiro dia, a segunda passa a ser o primeiro dia.
+*/
+set language 'us_english'
+declare @inputdate as date,
+		@compensatedate as tinyint,
+		@diffdate as date
+
+set @inputdate = SYSDATETIME()
+set @compensatedate = @@DATEFIRST
+
+select DATEPART(weekday, DATEADD(day, @compensatedate - 1, @inputdate)) as english_1;
+go
+
+
+-- teste com o idioma Inglês britânico
+set language 'British English';
+declare @inputdate as date,
+		@compensatedate as tinyint,
+		@diffdate as date
+
+set @inputdate = SYSDATETIME()
+set @compensatedate = @@DATEFIRST
+
+select DATEPART(weekday, DATEADD(day, @compensatedate - 1, @inputdate)) as britsh_1;
+go
+
+select @@DATEFIRST;
+select * from sys.syslanguages;
+select datepart(weekday, dateadd(day, @@datefirst - 1, '2024-01-23'))
+
+set language 'english'
+select @@DATEFIRST as firstday, datename(WEEKDAY, @@DATEFIRST) beginweek, DATEPART(weekday, SYSDATETIME())as today;
+
+set language 'British English';
+select @@DATEFIRST as firstday, datename(WEEKDAY, @@DATEFIRST) beginweek, DATEPART(weekday, SYSDATETIME())as today;
